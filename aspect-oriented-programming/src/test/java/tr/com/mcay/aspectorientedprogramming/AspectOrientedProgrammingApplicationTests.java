a2eadb9158e532f621e234180d600a4dee8b6db7
MUSTAFA ÇAY <mcay51@gmail.com>
2024-10-04 23:04:57 +0300

    Aspect Oriented Programming first commit

---
+++ aspect-oriented-programming/src/test/java/tr/com/mcay/aspectorientedprogramming/AspectOrientedProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.aspectorientedprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOrientedProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
