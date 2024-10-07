0da89719478c40dfd816b3c34c52fd567d0b09cd
MUSTAFA ÇAY <mcay51@gmail.com>
2024-10-07 23:54:49 +0300

    @Configuration anotasyonu

--- spring-annotations/src/main/java/tr/com/mcay/springannotations/config/ClassConditionConfig.java
+++ spring-annotations/src/main/java/tr/com/mcay/springannotations/config/ClassConditionConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @Configuration: Konfigürasyon sınıfını belirtir.
+ * @Configuration, Spring'e bu sınıfın bir konfigürasyon sınıfı olduğunu belirtir ve bu sınıfta tanımlanan metodlar @Bean anotasyonu ile işaretlenir.
+ * Bu sayede Spring, bu sınıfı uygulama başlatılırken kullanır ve gerekli bean'leri yaratır.
  * @ConditionalOnClass: Bu anotasyon, yalnızca belirli bir sınıf classpath'te mevcut olduğunda bean'in yaratılmasını sağlar.
  * Özellikle dış bağımlılıklara göre (örneğin, bir kütüphane yüklü mü değil mi) davranış sergilemek için kullanılır.
  * Bu bean, 'com.fasterxml.jackson.databind.ObjectMapper' sınıfı mevcutsa yaratılacak.
