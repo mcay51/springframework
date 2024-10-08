8d3ed18cf36c91636198b8327ffca719ef588681
MUSTAFA ÇAY <mcay51@gmail.com>
2024-10-08 22:59:23 +0300

    @PostConstruct @PreDestroy

--- spring-annotations/src/main/java/tr/com/mcay/springannotations/components/FirstComponent.java
+++ spring-annotations/src/main/java/tr/com/mcay/springannotations/components/FirstComponent.java
@@ -1,5 +1,7 @@
 package tr.com.mcay.springannotations.components;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +12,27 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(1)
 public class FirstComponent {
-
+    /**
+     * @PostConstruct anotasyonu, bir bean’in yaşam döngüsünde "başlatıldıktan hemen sonra" çalıştırılacak bir metodu tanımlar.
+     * Bean, bağımlılıklarının enjekte edilmesinden sonra, yani bean başlatıldıktan sonra bu metod çağrılır.
+     */
+    @PostConstruct
+    public void init() {
+        // Bean başlatıldıktan hemen sonra yapılacak işlemler
+        System.out.println("FirstComponent Initialization başlatıldı ve veritabanı bağlantısı kuruldu!");
+    }
 
     public String getMessage() {
         return "Order Birinci Bileşen Mesajı";
+    }
+
+    /**
+     * @PreDestroy anotasyonu, bir bean’in yok edilmeden önce çalıştırılacak olan metodu tanımlar.
+     * Bu metod, bean sonlandırılmadan hemen önce çağrılır ve temizleme işlemleri (örn. kaynakları serbest bırakma) için kullanılır.
+     */
+    @PreDestroy
+    public void cleanup() {
+        // Bean yok edilmeden önce yapılacak işlemler
+        System.out.println("CleanupService bean sonlandırılıyor, kaynaklar serbest bırakılıyor!");
     }
 }
