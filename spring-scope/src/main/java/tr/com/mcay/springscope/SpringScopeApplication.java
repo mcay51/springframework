48fcc3e898934ee3a32f885b276b65c5db33d9db
MUSTAFA ÇAY <mcay51@gmail.com>
2024-10-24 22:49:58 +0300

    spring boot scopes

---
+++ spring-scope/src/main/java/tr/com/mcay/springscope/SpringScopeApplication.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.springscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScopeApplication.class, args);
+    }
+
+}
