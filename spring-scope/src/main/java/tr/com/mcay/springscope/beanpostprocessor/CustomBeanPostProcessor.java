b1f7a1f9d1745aafa709aced4c12652c6ba4f351
MUSTAFA ÇAY <mcay51@gmail.com>
2024-10-24 22:56:41 +0300

    BeanPostProcessor Beanlerin yüklenme durumlarını kontrol için eklendi.

---
+++ spring-scope/src/main/java/tr/com/mcay/springscope/beanpostprocessor/CustomBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package tr.com.mcay.springscope.beanpostprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Before Initialization: " + beanName);
+        return bean; // Bean'ı döndürün (üzerinde değişiklik yapmadığınız sürece)
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("After Initialization: " + beanName);
+        return bean; // Bean'ı döndürün (üzerinde değişiklik yapmadığınız sürece)
+    }
+}
