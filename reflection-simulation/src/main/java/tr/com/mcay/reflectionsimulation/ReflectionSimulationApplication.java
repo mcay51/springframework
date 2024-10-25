b14ed31cb78fe83d4fbdb76c84eccc8a0c6ab737
MUSTAFA ÇAY <mcay51@gmail.com>
2024-10-25 22:53:21 +0300

    first commit

---
+++ reflection-simulation/src/main/java/tr/com/mcay/reflectionsimulation/ReflectionSimulationApplication.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.reflectionsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectionSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReflectionSimulationApplication.class, args);
+    }
+
+}
---
+++ reflection-simulation/src/test/java/tr/com/mcay/reflectionsimulation/ReflectionSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.reflectionsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
